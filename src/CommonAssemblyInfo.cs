4434e5c0fd4f0add25f9cd672d2f265647b80f85
Sergej Derjabkin <sergej@derjabkin.de>
2014-11-11 12:56:38 +0100

    - CommonAssemblyInfo.cs: Version 1.4.0.0

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("http://fluentnhibernate.org")]
 [assembly: AssemblyProduct("FluentNHibernate")]
 [assembly: AssemblyCopyright("Copyright 2008-2013 James Gregory and contributors (Paul Batum, Hudson Akridge et al). All rights reserved.")]
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
 
 [assembly: AllowPartiallyTrustedCallers()]
